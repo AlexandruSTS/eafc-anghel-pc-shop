2a398af2fb9e12815df3ead45f3a0f7a0644493e
Alexandru Anghel <alexandru@ditar.io>
2023-06-06 13:55:54 +0200

    security config url changed to api/auth

--- src/main/java/eafcanghel/pcshop/security/config/SecurityConfig.java
+++ src/main/java/eafcanghel/pcshop/security/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**")
+                .requestMatchers("/api/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
