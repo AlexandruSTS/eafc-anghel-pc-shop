b5da8e65b5f96f844620c1b87a37b951861b4fca
Alexandru Anghel <alexandru@ditar.io>
2023-06-12 23:50:51 +0200

    Pagination second working version with data inserts

---
+++ src/main/resources/db/migration/V1_0_7__insert_items_and_categories.sql
@@ -0,0 +1,219 @@
+-- Inserting items
+INSERT INTO ITEM (NAME, DESCRIPTION, PRICE)
+VALUES
+    ('HP Pavilion', 'Powerful desktop PC for home and office use', 799.99),
+    ('Dell Inspiron', 'Versatile laptop for everyday computing tasks', 699.99),
+    ('Apple iMac', 'Sleek and stylish all-in-one computer', 1299.99),
+    ('Lenovo ThinkPad', 'Business-grade laptop with excellent durability', 1099.99),
+    ('AMD Ryzen 7', 'High-performance CPU for gaming and productivity', 349.99),
+    ('Samsung 1TB', 'Reliable internal hard drive for storage needs', 89.99),
+    ('NVIDIA GeForce RTX 3080', 'Top-tier graphics card for gaming enthusiasts', 799.99),
+    ('LG UltraWide', 'Immersive ultrawide monitor for enhanced productivity', 599.99),
+    ('Logitech G Pro', 'Mechanical gaming keyboard with customizable RGB lighting', 129.99),
+    ('Razer DeathAdder', 'Ergonomic gaming mouse with high precision', 59.99),
+    ('Asus ROG Strix', 'Gaming laptop with powerful performance', 1499.99),
+    ('Apple MacBook Air', 'Thin and lightweight laptop for on-the-go productivity', 999.99),
+    ('Intel Core i9', 'Flagship processor for extreme performance', 599.99),
+    ('Seagate 2TB', 'High-capacity external hard drive for data backup', 129.99),
+    ('AMD Radeon RX 6800 XT', 'High-end graphics card for immersive gaming', 699.99),
+    ('Samsung Odyssey G9', 'Curved gaming monitor with QLED display', 1499.99),
+    ('Corsair K95 RGB Platinum', 'Premium mechanical gaming keyboard with advanced features', 199.99),
+    ('SteelSeries Rival 600', 'Ergonomic gaming mouse with customizable weight system', 79.99),
+    ('HP Spectre x360', 'Convertible laptop with sleek design and powerful performance', 1299.99),
+    ('Dell XPS 15', 'Premium ultrabook with stunning display and powerful hardware', 1499.99),
+    ('Apple Mac Pro', 'High-performance workstation for professionals', 5999.99),
+    ('AMD Ryzen 5', 'Mid-range processor for balanced performance', 249.99),
+    ('Western Digital 4TB', 'External hard drive with large storage capacity', 119.99),
+    ('NVIDIA GeForce GTX 1660 Ti', 'Graphics card for smooth gaming at 1080p', 299.99),
+    ('Acer Predator X34', 'Curved ultrawide gaming monitor with G-Sync technology', 999.99),
+    ('Logitech MX Keys', 'Wireless keyboard with comfortable typing experience', 99.99),
+    ('Razer Basilisk', 'Ergonomic gaming mouse with customizable scroll wheel', 69.99),
+    ('Asus ROG G14', 'Portable gaming laptop with powerful performance', 1499.99),
+    ('Apple iMac Pro', 'Professional-grade all-in-one desktop for creative work', 4999.99),
+    ('Intel Core i7', 'High-performance processor for gaming and multitasking', 399.99),
+    ('Seagate 500GB', 'Solid-state drive for faster data access and boot times', 79.99),
+    ('NVIDIA GeForce RTX 3070', 'Graphics card for high-quality gaming at 1440p', 499.99),
+    ('LG 27UK850-W', '4K monitor with accurate colors and USB-C connectivity', 499.99),
+    ('Corsair K70 RGB', 'Mechanical gaming keyboard with customizable backlighting', 139.99),
+    ('Logitech G502', 'Wired gaming mouse with adjustable sensitivity and weights', 79.99),
+    ('Lenovo Yoga C940', 'Premium convertible laptop with versatile features', 1299.99),
+    ('Dell Alienware Aurora', 'Powerful gaming desktop with customizable configuration', 1999.99),
+    ('Apple MacBook Pro', 'High-performance laptop for professional users', 1799.99),
+    ('AMD Ryzen 3', 'Budget-friendly processor for entry-level gaming and tasks', 149.99),
+    ('Samsung 500GB', 'Internal solid-state drive for faster data transfer speeds', 99.99),
+    ('NVIDIA GeForce GTX 1650', 'Graphics card for casual gaming and multimedia tasks', 199.99),
+    ('HP OMEN X 35', 'Curved gaming monitor with ultra-wide aspect ratio', 999.99),
+    ('Razer Huntsman Elite', 'Mechanical gaming keyboard with opto-mechanical switches', 199.99),
+    ('SteelSeries Sensei Ten', 'Ambidextrous gaming mouse with TrueMove Pro sensor', 69.99),
+    ('Asus ZenBook 14', 'Thin and lightweight laptop with excellent battery life', 899.99),
+    ('Apple Mac Mini', 'Compact desktop computer for various computing needs', 699.99),
+    ('Intel Core i5', 'Mid-range processor for balanced performance', 299.99),
+    ('Western Digital 1TB', 'Portable external hard drive for on-the-go storage', 59.99),
+    ('NVIDIA GeForce RTX 3060 Ti', 'Graphics card for high-quality gaming at 1440p', 399.99),
+    ('Dell UltraSharp U3415W', 'Curved ultrawide monitor for enhanced productivity', 799.99),
+    ('Logitech G915', 'Wireless mechanical gaming keyboard with low-profile switches', 249.99),
+    ('Razer Naga Trinity', 'Modular gaming mouse with interchangeable side plates', 99.99),
+    ('Lenovo Legion 5', 'Gaming laptop with powerful performance and immersive display', 1199.99),
+    ('Acer Aspire 5', 'Budget-friendly laptop for everyday use', 599.99),
+    ('MSI GL65 Leopard', 'Gaming laptop with powerful performance', 1199.99),
+    ('Lenovo ThinkCentre M720', 'Compact desktop computer for business use', 799.99),
+    ('Samsung 970 EVO Plus', 'High-speed NVMe SSD for fast data storage', 129.99),
+    ('ASRock B450M PRO4', 'Micro ATX motherboard for AMD processors', 89.99),
+    ('HP Pavilion Gaming Mouse 300', 'Wired gaming mouse with customizable DPI', 49.99),
+    ('Dell G5 Gaming Desktop', 'Gaming desktop with sleek design', 1499.99),
+    ('G.Skill Trident Z RGB', 'RGB DDR4 RAM for enhanced aesthetics', 199.99),
+    ('LG 34UC79G-B', 'UltraWide gaming monitor with FreeSync technology', 699.99),
+    ('Logitech G Pro X Mechanical', 'High-performance mechanical gaming keyboard', 129.99),
+    ('Apple iMac 24', 'All-in-one desktop computer with stunning Retina display', 1699.99),
+    ('Corsair Vengeance LPX', 'High-performance DDR4 RAM for gaming', 149.99),
+    ('Seagate BarraCuda 2TB', 'Internal hard drive for high-capacity storage', 79.99),
+    ('AMD Radeon RX 6700 XT', 'Graphics card for high-quality gaming and content creation', 499.99),
+    ('Lenovo ThinkPad X1 Carbon Gen 9', 'Premium business laptop with ultra-slim design', 1799.99),
+    ('Razer BlackWidow Elite', 'Mechanical gaming keyboard with customizable lighting', 169.99),
+    ('Western Digital Blue 1TB', 'Internal SSD for reliable and fast storage', 109.99),
+    ('MSI MAG241C', 'Curved gaming monitor with a fast refresh rate', 299.99),
+    ('ASUS ROG Swift PG279QZ', '27-inch gaming monitor with IPS panel and G-Sync', 699.99),
+    ('Logitech MX Master 3', 'Wireless mouse with advanced features and precision', 99.99),
+    ('Acer Predator Helios 300', 'Gaming laptop with high-performance components', 1299.99),
+    ('Dell XPS 13', 'Ultrabook with premium design and high-resolution display', 1399.99),
+    ('Samsung 860 EVO 500GB', 'SATA SSD for reliable and fast storage', 79.99),
+    ('AMD Ryzen 9 5900X', 'High-end processor for gaming and content creation', 549.99),
+    ('Gigabyte Aorus FI27Q', '27-inch gaming monitor with QHD resolution and high refresh rate', 699.99),
+    ('HP OMEN Sequencer', 'Mechanical gaming keyboard with customizable lighting', 149.99),
+    ('Crucial Ballistix 16GB', 'High-performance DDR4 RAM for gaming and overclocking', 129.99),
+    ('Seagate FireCuda 2TB', 'Hybrid SSHD for fast storage and quick load times', 99.99),
+    ('NVIDIA GeForce RTX 3070 Ti', 'Powerful graphics card for high-end gaming and rendering', 799.99),
+    ('Lenovo Yoga 9i', 'Convertible laptop with premium design and touchscreen', 1599.99),
+    ('Razer DeathAdder V2', 'Ergonomic gaming mouse with high-precision sensor', 69.99),
+    ('ASUS TUF Gaming VG27AQ', '27-inch gaming monitor with WQHD resolution and Adaptive Sync', 399.99),
+    ('Apple MacBook Air M1', 'Ultra-portable laptop with Apple Silicon chip', 999.99),
+    ('Corsair K70 RGB MK.2', 'Mechanical gaming keyboard with per-key RGB lighting', 159.99),
+    ('Western Digital Black 2TB', 'High-performance NVMe SSD for demanding applications', 299.99),
+    ('NVIDIA GeForce GTX 1660 Super', 'Graphics card for smooth 1080p gaming performance', 299.99),
+    ('HP ENVY 15', 'Premium laptop with powerful performance and stunning display', 1499.99),
+    ('MSI Optix MAG274QRF-QD', '27-inch gaming monitor with QHD resolution and fast refresh rate', 499.99),
+    ('ASUS ROG Strix Scope', 'Mechanical gaming keyboard with compact design and Aura Sync', 129.99),
+    ('Samsung 870 QVO 2TB', 'SATA SSD with high capacity for mass storage', 189.99),
+    ('AMD Ryzen 7 5800X', 'High-performance processor for gaming and multi-threaded tasks', 399.99),
+    ('AOC CQ32G1', '32-inch curved gaming monitor with QHD resolution', 399.99),
+    ('Logitech G305 Lightspeed', 'Wireless gaming mouse with high-precision sensor', 59.99),
+    ('Acer Aspire TC', 'Desktop computer for home and office use', 699.99),
+    ('Crucial MX500 1TB', 'SATA SSD for reliable and affordable storage', 99.99),
+    ('NVIDIA GeForce GTX 1650 Super', 'Graphics card for budget-friendly gaming performance', 199.99),
+    ('ASUS ROG Swift PG348Q', '34-inch ultra-wide gaming monitor with curved display', 899.99),
+    ('Ducky One 2 Mini', 'Compact mechanical gaming keyboard with customizable lighting', 119.99),
+    ('Samsung T5 1TB', 'Portable SSD for fast data transfer and backup', 149.99),
+    ('Intel Core i9-11900K', 'High-end processor for extreme gaming and content creation', 599.99),
+    ('Gigabyte Aorus AD27QD', '27-inch gaming monitor with QHD resolution and HDR', 599.99),
+    ('Razer Viper Ultimate', 'Wireless gaming mouse with high-precision and lightweight design', 149.99);
+
+
+-- Inserting categories
+INSERT INTO CATEGORY (NAME, DESCRIPTION)
+VALUES
+    ('PC', 'Personal computer systems'),
+    ('Mac', 'Apple Macintosh computers'),
+    ('Laptop', 'Portable computing devices'),
+    ('CPU', 'Central Processing Units'),
+    ('HDD', 'Hard Disk Drives'),
+    ('RAM', 'Random Access Memory'),
+    ('GPU', 'Graphics Processing Units'),
+    ('Screen', 'Computer monitors and displays'),
+    ('Keyboard', 'Input devices for typing'),
+    ('Mouse', 'Pointing devices for cursor control');
+
+-- Inserting item-category associations
+-- Inserting item-category associations
+INSERT INTO ITEM_CATEGORIES (ITEM_ID, CATEGORIES_ID)
+SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('HP Pavilion', 'Dell Inspiron', 'Apple iMac', 'Lenovo ThinkPad', 'AMD Ryzen 7')
+  AND C.NAME IN ('PC', 'Laptop')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Samsung 1TB', 'NVIDIA GeForce RTX 3080', 'LG UltraWide', 'Logitech G Pro', 'Razer DeathAdder')
+  AND C.NAME IN ('HDD', 'GPU')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Asus ROG Strix', 'Apple MacBook Air', 'Intel Core i9', 'Seagate 2TB', 'AMD Radeon RX 6800 XT')
+  AND C.NAME IN ('Laptop', 'CPU')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Samsung Odyssey G9', 'Corsair K95 RGB Platinum', 'SteelSeries Rival 600', 'HP Spectre x360', 'Dell XPS 15')
+  AND C.NAME IN ('Screen', 'Keyboard')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Apple Mac Pro', 'AMD Ryzen 5', 'Western Digital 4TB', 'NVIDIA GeForce GTX 1660 Ti', 'Acer Predator X34')
+  AND C.NAME IN ('PC', 'HDD')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Logitech MX Keys', 'Razer Basilisk', 'Asus ROG G14', 'Apple iMac Pro', 'Intel Core i7')
+  AND C.NAME IN ('Keyboard', 'Mac')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Seagate 500GB', 'NVIDIA GeForce RTX 3070', 'LG 27UK850-W', 'Corsair K70 RGB', 'Logitech G502')
+  AND C.NAME IN ('HDD', 'GPU')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Lenovo Yoga C940', 'Dell Alienware Aurora', 'Apple MacBook Pro', 'AMD Ryzen 3', 'Samsung 500GB')
+  AND C.NAME IN ('Laptop', 'CPU')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('NVIDIA GeForce GTX 1650', 'HP OMEN X 35', 'Razer Huntsman Elite', 'SteelSeries Sensei Ten', 'Asus ZenBook 14')
+  AND C.NAME IN ('GPU', 'Screen')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Apple Mac Mini', 'Intel Core i5', 'Western Digital 1TB', 'NVIDIA GeForce RTX 3060 Ti', 'Dell UltraSharp U3415W')
+  AND C.NAME IN ('PC', 'Screen')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Logitech G915', 'Razer Naga Trinity', 'Lenovo Legion 5')
+  AND C.NAME IN ('Keyboard', 'Mouse');
+
+
+INSERT INTO ITEM_CATEGORIES (ITEM_ID, CATEGORIES_ID)
+SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Acer Aspire 5', 'MSI GL65 Leopard', 'Lenovo ThinkCentre M720', 'Samsung 970 EVO Plus', 'ASRock B450M PRO4')
+  AND C.NAME IN ('Laptop', 'PC')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('HP Pavilion Gaming Mouse 300', 'Dell G5 Gaming Desktop', 'G.Skill Trident Z RGB', 'LG 34UC79G-B', 'Logitech G Pro X Mechanical')
+  AND C.NAME IN ('Mouse', 'Screen')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Apple iMac 24', 'Corsair Vengeance LPX', 'Seagate BarraCuda 2TB', 'AMD Radeon RX 6700 XT', 'Lenovo ThinkPad X1 Carbon Gen 9')
+  AND C.NAME IN ('Mac', 'Laptop')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Razer BlackWidow Elite', 'Western Digital Blue 1TB', 'MSI MAG241C', 'ASUS ROG Swift PG279QZ', 'Logitech MX Master 3')
+  AND C.NAME IN ('Keyboard', 'Screen')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Acer Predator Helios 300', 'Dell XPS 13', 'Samsung 860 EVO 500GB', 'AMD Ryzen 9 5900X', 'Gigabyte Aorus FI27Q')
+  AND C.NAME IN ('Laptop', 'CPU')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('HP OMEN Sequencer', 'Crucial Ballistix 16GB', 'Seagate FireCuda 2TB', 'NVIDIA GeForce RTX 3070 Ti', 'Lenovo Yoga 9i')
+  AND C.NAME IN ('Keyboard', 'GPU')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Razer DeathAdder V2', 'ASUS TUF Gaming VG27AQ', 'Apple MacBook Air M1', 'Corsair K70 RGB MK.2', 'Western Digital Black 2TB')
+  AND C.NAME IN ('Mouse', 'Screen')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('NVIDIA GeForce GTX 1660 Super', 'HP ENVY 15', 'MSI Optix MAG274QRF-QD', 'ASUS ROG Strix Scope', 'Samsung 870 QVO 2TB')
+  AND C.NAME IN ('GPU', 'Laptop')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('AMD Ryzen 7 5800X', 'AOC CQ32G1', 'Logitech G305 Lightspeed', 'Acer Aspire TC', 'Crucial MX500 1TB')
+  AND C.NAME IN ('CPU', 'Mouse')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('NVIDIA GeForce GTX 1650 Super', 'ASUS ROG Swift PG348Q', 'Ducky One 2 Mini', 'Samsung T5 1TB', 'Intel Core i9-11900K')
+  AND C.NAME IN ('GPU', 'Keyboard')
+UNION ALL SELECT I.ID, C.ID
+FROM ITEM I, CATEGORY C
+WHERE I.NAME IN ('Gigabyte Aorus AD27QD', 'Razer Viper Ultimate')
+  AND C.NAME IN ('Screen', 'Mouse');
+
