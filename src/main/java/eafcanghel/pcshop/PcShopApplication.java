40de4fd29c30e541ea0585e56a58cd2f5f524efa
Alexandru Anghel <alexandru@ditar.io>
2023-06-20 17:09:07 +0200

    first deployment

--- src/main/java/eafcanghel/pcshop/PcShopApplication.java
+++ src/main/java/eafcanghel/pcshop/PcShopApplication.java
@@ -8,5 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PcShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(PcShopApplication.class, args);
+
     }
 }
